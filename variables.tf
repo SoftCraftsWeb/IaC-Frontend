6abf17eec3b685e464b849999624290110d5aa75
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-08-06 21:59:17 -0500

    refactor: Add variable domain name and fix description

--- variables.tf
+++ variables.tf
@@ -1,10 +1,17 @@
+variable "region" {  
+  type        = string  
+  default     = "us-east-1"  
+  description = "The AWS Region to use."
+}
+
 variable "bucket_prefix" {  
   type        = string  
   default     = "oldwave.cf"
   description = "Name of the s3 bucket to be created."
 } 
-variable "region" {  
-  type        = string  
-  default     = "us-east-1"  
-  description = "Name of the s3 bucket to be created."
+
+variable "domain_name" {
+  type        = string
+  description = "The domain name to use."
+  default     = "oldwave"
 }
